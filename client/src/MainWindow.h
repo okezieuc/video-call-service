afcca85b6df8d305381d4159a97c8d57591c1c69
okezieuc <53785400+okezieuc@users.noreply.github.com>
2026-02-21 19:19:46 -0600

    add camera access support
    user can now see camera preview in the main app

--- client/src/MainWindow.h
+++ client/src/MainWindow.h
@@ -1,8 +1,29 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QMediaCaptureSession>
+#include <QPermissions>
+
+class QCamera;
+class QCameraPermission;
+class QPushButton;
+class QVBoxLayout;
+class QVideoWidget;
+class QWidget;
 
 class MainWindow : public QMainWindow {
 public:
     explicit MainWindow(QWidget* parent = nullptr);
+private:
+    // Layout
+    QWidget* central = nullptr;
+    QVBoxLayout* layout = nullptr;
+    QPushButton* joinCallButton = nullptr;
+
+    // Camera-related
+    QCamera* camera = nullptr;
+    QMediaCaptureSession captureSession;
+    QVideoWidget* videoPreview = nullptr;
+    QCameraPermission cameraPermission; 
 };
+
