b49b21eae6cd30fb9de7e56c81d803a09f9753c3
okezieuc <53785400+okezieuc@users.noreply.github.com>
2026-03-12 09:34:07 -0500

    add tests for mainwindow camera status checks
    - created seams for system call methods so they can be faked in tests

--- client/src/MainWindow.h
+++ client/src/MainWindow.h
@@ -1,11 +1,15 @@
 #pragma once
 
+#include "utils.h"
 #include "FrameHandler.h"
 #include "VideoPreview.h"
 
 #include <QMainWindow>
 #include <QMediaCaptureSession>
 #include <QPermissions>
+
+// Forward declaration of mock class for tests
+class MockMainWindow;
 
 class QCamera;
 class QCameraPermission;
@@ -14,12 +18,24 @@ class QVBoxLayout;
 class QWidget;
 class VideoSink;
 
+struct CameraStatus {
+  Tristate isPermissionGranted = Tristate::Unknown;
+  Tristate deviceHasCamera = Tristate::Unknown;
+  bool isCameraActive = false;
+};
+
 class MainWindow : public QMainWindow {
+  friend class MockMainWindow;
+
 public:
   explicit MainWindow(QWidget *parent = nullptr);
+  void updateCameraStatus();
 
 protected:
-  virtual bool isCameraAvailable() const;
+  virtual Qt::PermissionStatus checkCameraPermissionStatus() const;
+  virtual void requestCameraPermission();
+  virtual void startCamera();
+  virtual int videoInputCount() const;
 
 private:
   // Layout
@@ -32,6 +48,8 @@ private:
   QMediaCaptureSession captureSession;
   QCameraPermission cameraPermission;
   QVideoSink *videoSink;
+
+  CameraStatus cameraStatus;
 
   FrameHandler *videoFrameHandler;
   VideoPreview *videoPreviewArea;
--- tests/client/MainWindowTest.cpp
+++ tests/client/MainWindowTest.cpp
@@ -1,9 +1,70 @@
 #include "MainWindow.h"
 #include <gtest/gtest.h>
 
+class MockMainWindow : public MainWindow {
+public:
+  int fakeVideoInputCount = 1;
+  Qt::PermissionStatus fakeCameraPermissionStatus =
+      Qt::PermissionStatus::Undetermined;
+  bool requestCameraPermissionCalled = false;
+  bool isCameraStarted = false;
+
+  auto getCameraStatus() { return cameraStatus; }
+
+protected:
+  Qt::PermissionStatus checkCameraPermissionStatus() const override {
+    return fakeCameraPermissionStatus;
+  }
+  void requestCameraPermission() override {
+    requestCameraPermissionCalled = true;
+  }
+  void startCamera() override {
+    isCameraStarted = true;
+  }
+  int videoInputCount() const override { return fakeVideoInputCount; }
+};
+
 class MainWindowTest : public testing::Test {
 protected:
   MainWindowTest() {}
 
-  MainWindow window;
+  MockMainWindow window;
 };
+
+TEST_F(MainWindowTest, UpdateStatus_WithNoVideoInput_SetsDeviceHasCameraFalse) {
+  window.fakeVideoInputCount = 0;
+  window.updateCameraStatus();
+  EXPECT_EQ(window.getCameraStatus().deviceHasCamera, Tristate::False);
+}
+
+TEST_F(MainWindowTest,
+       UpdateStatus_UpdatesCameraStatus) {
+  window.fakeCameraPermissionStatus = Qt::PermissionStatus::Granted;
+  window.fakeVideoInputCount = 1;
+  window.updateCameraStatus();
+  EXPECT_EQ(window.getCameraStatus().deviceHasCamera, Tristate::True);
+  EXPECT_EQ(window.getCameraStatus().isPermissionGranted, Tristate::True);
+  EXPECT_TRUE(window.isCameraStarted);
+}
+
+
+TEST_F(MainWindowTest,
+       UpdateStatus_WithNonZeroVideoInput_SetsDeviceHasCameraTrue) {
+  window.fakeVideoInputCount = 3;
+  window.updateCameraStatus();
+  EXPECT_EQ(window.getCameraStatus().deviceHasCamera, Tristate::True);
+}
+
+TEST_F(MainWindowTest,
+       UpdateStatus_WhenPermissionIsUndetermined_RequestsCameraPermission) {
+  window.fakeCameraPermissionStatus = Qt::PermissionStatus::Undetermined;
+  window.updateCameraStatus();
+  EXPECT_TRUE(window.requestCameraPermissionCalled);
+}
+
+TEST_F(MainWindowTest,
+       UpdateStatus_WhenPermissionIsDetermined_WontRequestCameraPermission) {
+  window.fakeCameraPermissionStatus = Qt::PermissionStatus::Granted;
+  window.updateCameraStatus();
+  EXPECT_FALSE(window.requestCameraPermissionCalled);
+}
